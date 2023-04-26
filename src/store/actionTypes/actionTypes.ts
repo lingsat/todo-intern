6f7d05302d14c7a76164f0071d9c66b4d4ca55c0
lingsat <lingsatpilingast@gmail.com>
2023-04-26 15:07:50 +0300

    ref: remove actions, action types

--- src/store/actionTypes/actionTypes.ts
+++
@@ -1,9 +0,0 @@
-export enum COURSES_ACTIONS {
-  ADD_TASK = "ADD_TASK",
-  DELETE_TASK = "DELETE_TASK",
-}
-
-export interface IAction {
-  type: COURSES_ACTIONS;
-  payload?: any;
-}
--- src/store/reducers/todoReducer.ts
+++ src/store/reducers/todoReducer.ts
@@ -1,19 +1,12 @@
-import { COURSES_ACTIONS, IAction } from "../actionTypes/actionTypes";
 import { ITask } from "../../types/task.interface";
 
 const initialState: ITask[] = [];
 
 export const todoReducer = (
   state: ITask[] = initialState,
-  action: IAction
+  action: { type: string }
 ): ITask[] => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case COURSES_ACTIONS.ADD_TASK:
-      return [...payload, ...state];
-    case COURSES_ACTIONS.DELETE_TASK:
-      return [...state.filter((task: ITask) => task.id !== payload)];
+  switch (action.type) {
     default:
       return state;
   }
