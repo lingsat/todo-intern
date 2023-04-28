c284b9e5a095840d8df903b824490620001e9d71
lingsat <lingsatpilingast@gmail.com>
2023-04-28 13:46:58 +0300

    feat: add checkbox, add done mark, cross line on text in done task

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,20 +1,48 @@
 import React, { FC } from "react";
+import { useDispatch } from "react-redux";
 import { getValidDateStr } from "../../utils/date.utils";
+import { TodoActionTypes } from "../../store/actionTypes/actionTypes";
 import styles from "./TodoItem.module.scss";
 
 interface TodoItemProps {
+  id: string;
   title: string;
   createdDate: Date;
   expiredDate: Date;
+  completed: boolean;
 }
 
-const TodoItem: FC<TodoItemProps> = ({ title, createdDate, expiredDate }) => {
+const TodoItem: FC<TodoItemProps> = ({
+  id,
+  title,
+  createdDate,
+  expiredDate,
+  completed,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleCheckboxChange = () => {
+    dispatch({ type: TodoActionTypes.TOGGLE_COMPLETE, payload: id });
+  };
   return (
     <li className={styles.item}>
-      <h3 className={styles.title}>{title}</h3>
-      <div>
-        <p className={styles.text}>Created: {getValidDateStr(createdDate)}</p>
-        <p className={styles.text}>Expired: {getValidDateStr(expiredDate)}</p>
+      <div className={styles.completed}>
+        {completed && <div className={styles.status}>Done</div>}
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          checked={completed}
+          onChange={handleCheckboxChange}
+          name="completed"
+          id="completed"
+        />
+      </div>
+      <div className={`${styles.content} ${completed && styles.crosed}`}>
+        <h3 className={styles.title}>{title}</h3>
+        <div>
+          <p className={styles.text}>Created: {getValidDateStr(createdDate)}</p>
+          <p className={styles.text}>Expired: {getValidDateStr(expiredDate)}</p>
+        </div>
       </div>
     </li>
   );
