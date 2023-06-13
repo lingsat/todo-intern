25658e881c6125990245f73b1da31d775f954d72
lingsat <lingsatpilingast@gmail.com>
2023-06-13 07:41:01 +0300

    feat: add task controller

--- server/src/controllers/task.ts
+++ server/src/controllers/task.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import { Task } from '../models/task.js';
+import { ReqAddTaskBody } from '../types/request.js';
 
 export const getTaskList = async (req: Request, res: Response) => {
   const { id } = req.body.user;
@@ -9,5 +10,16 @@ export const getTaskList = async (req: Request, res: Response) => {
     res.status(200).json(taskList);
   } catch (error) {
     res.status(500).json({ message: 'Todos uploading fails!' });
+  }
+};
+
+export const addTask = async (req: ReqAddTaskBody, res: Response) => {
+  const { title, createdDate, expiredDate } = req.body;
+  const { id } = req.body.user;
+  try {
+    const task = await Task.create({ userId: id, title, createdDate, expiredDate });
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: 'Task creation fails!' });
   }
 };
--- server/src/types/request.ts
+++ server/src/types/request.ts
@@ -1,8 +1,19 @@
 import { Request } from 'express';
 
+import { IUser } from './user.js';
+
 export interface ReqUserBody extends Request {
   body: {
     email: string;
     password: string;
+  };
+}
+
+export interface ReqAddTaskBody extends Request {
+  body: {
+    title: string;
+    createdDate: string;
+    expiredDate: string;
+    user: IUser;
   };
 }
