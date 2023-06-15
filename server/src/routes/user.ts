3f93fa88942f5bd195aadc55d6b27a71a541e80b
lingsat <lingsatpilingast@gmail.com>
2023-06-15 16:18:31 +0300

    feat: return new tokens on valid refresh token request

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { loginUser } from '../controllers/user.js';
+import { loginUser, getNewTokens } from '../controllers/user.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', loginUser);
+userRouter.post('/refresh', getNewTokens);
 
 export default userRouter;
